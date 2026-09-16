ec39ba977f0a6b443495b4275ed2db87302cda98
stinetorndal <cph-sc95@stud.ek.dk>
2026-09-16 16:44:44 +0200

    dtos created

--- src/main/java/app/dtos/MovieDTO.java
+++ src/main/java/app/dtos/MovieDTO.java
@@ -1,4 +1,31 @@
 package app.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class MovieDTO {
+    private Long id;
+    private String title;
+    private String overview;
+    private String release_date;
+
+    @JsonProperty("vote_average")
+    private double averageRating;
+    private List<DirectorDTO> director;
+    private List<ActorDTO> actor;
+    private List<GenreDTO> genres;
+
+
 }
+
+
