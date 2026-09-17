ca6c87e5fc0030d3425561d75e8cf3959f3042ea
KatarinaKN <katarinak.nielsen@gmail.com>
2026-09-17 12:46:10 +0200

    Created MovieSearchResultDTO with totalPages and movieIds attributes

--- src/main/java/app/dtos/MovieSearchResultDTO.java
+++ src/main/java/app/dtos/MovieSearchResultDTO.java
@@ -1,4 +1,20 @@
 package app.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MovieSearchResultDTO {
+
+    private List<MovieIdDTO> movieIds;
+    @JsonProperty("total_pages")
+    private int totalPages;
 }
