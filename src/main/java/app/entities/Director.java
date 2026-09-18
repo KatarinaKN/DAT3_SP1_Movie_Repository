6e045fd8664202eab5fdbc61b1820a24968c72bd
KatarinaKN <katarinak.nielsen@gmail.com>
2026-09-18 16:08:06 +0200

    Director: added AllArgsConstructor and deleted white space

--- src/main/java/app/entities/Director.java
+++ src/main/java/app/entities/Director.java
@@ -3,6 +3,7 @@ package app.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -11,11 +12,11 @@ import lombok.ToString;
 @Table(name="directors")
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @ToString
 public class Director implements IEntity {
     @Id
     private int id;
-
     private String name;
 
     @Override
