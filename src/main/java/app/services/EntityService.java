b6279998579b29c23aad37b77d453efb5b3b64f7
KatarinaKN <katarinak.nielsen@gmail.com>
2026-09-18 16:09:29 +0200

    EntityService: added methods for converting dtos to entities.

--- src/main/java/app/services/EntityService.java
+++ src/main/java/app/services/EntityService.java
@@ -1,43 +1,72 @@
 package app.services;
 
-import app.dtos.ActorDTO;
-import app.dtos.GenreDTO;
-import app.dtos.MovieDTO;
+import app.dtos.*;
 import app.entities.Actor;
+import app.entities.Director;
 import app.entities.Genre;
 import app.entities.Movie;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class EntityService {
 
-    //Vi har dto'er
-    //Dto'er skal laves til entiteter.
-    //MovieDTO indeholder data til Movie, Genre, Actor.
+    private final MovieService movieService = new MovieService();
+    private final List<MovieDTO> movieDTOS = movieService.getMovies();
 
-  /*  public Movie convertToMovie(MovieDTO movieDTO) {
+    public List<Movie> convertToMovieEntities() {
+        List<Movie> movies = new ArrayList<>();
 
-        List<ActorDTO> actorDTOS = movieDTO.getCredits().getCast();
-        List<Actor> actors = new ArrayList<>();
+        for (MovieDTO movieDTO : movieDTOS) {
+            int movieId = movieDTO.getId();
+            String title = movieDTO.getTitle();
+            double popularity = movieDTO.getPopularity();
+            LocalDate releaseDate = movieDTO.getReleaseDate();
+            double averageRating = movieDTO.getAverageRating();
 
-        for (ActorDTO actorDTO : actorDTOS) {
-            Actor actor = new Actor(actorDTO.getId(), actorDTO.getName());
-            actors.add(actor);
+            Set<Genre> genres = convertToGenreEntities(movieDTO);
+            Set<Actor> actors = convertToActorEntities(movieDTO);
+            Director director = convertToDirectorEntity(movieDTO);
+
+            Movie movie = new Movie(movieId, title, releaseDate, popularity, averageRating, director, actors, genres);
+            movies.add(movie);
         }
+        return movies;
+    }
 
+    private Set<Genre> convertToGenreEntities(MovieDTO movieDTO) {
         List<GenreDTO> genreDTOS = movieDTO.getGenres();
-        List<Genre> genres = new ArrayList<>();
-
+        Set<Genre> genres = new HashSet<>();
         for (GenreDTO genreDTO : genreDTOS) {
             Genre genre = new Genre(genreDTO.getId(), genreDTO.getName());
             genres.add(genre);
         }
+        return genres;
+    }
 
+    private Set<Actor> convertToActorEntities(MovieDTO movieDTO) {
+        List<ActorDTO> actorDTOS = movieDTO.getCredits().getCast();
+        Set<Actor> actors = new HashSet<>();
+        for (ActorDTO actorDTO : actorDTOS) {
+            Actor actor = new Actor(actorDTO.getId(), actorDTO.getName());
+            actors.add(actor);
+        }
+        return actors;
+    }
 
+    private Director convertToDirectorEntity(MovieDTO movieDTO) {
+        List<CrewMemberDTO> crewMemberDTOS = movieDTO.getCredits().getCrew();
+        int directorDTOId = crewMemberDTOS.stream()
+                .filter(crewMemberDTO -> "Director".equals(crewMemberDTO.getJob()))
+                .mapToInt(crewMemberDTO -> crewMemberDTO.getId()).sum();
 
-        //Director
-        //Movie movie  = new Movie (actor, genre, director
-        return
-    } */
+        String directorDTOName = crewMemberDTOS.stream()
+                .filter(crewMemberDTO -> "Director".equals(crewMemberDTO.getJob()))
+                .map(crewMemberDTO -> crewMemberDTO.getName()).toString();
+
+        return new Director(directorDTOId, directorDTOName);
+    }
 }
