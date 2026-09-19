c343c8a31cf603dfa51b68179ecb3448e2fd0356
KatarinaKN <katarinak.nielsen@gmail.com>
2026-09-19 16:55:29 +0200

    MovieDAO: removed unused import.

--- src/main/java/app/dao/MovieDAO.java
+++ src/main/java/app/dao/MovieDAO.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class MovieDAO extends GenericDAO<Movie, Integer> {
     public MovieDAO(EntityManagerFactory emf) {
