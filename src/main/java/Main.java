965226b46c7086995ca744a6c2e8b45fe234bdc2
KatarinaKN <katarinak.nielsen@gmail.com>
2026-09-19 17:05:02 +0200

    Main: removed mess and unused imports.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,9 @@
 import app.AppRunner;
-import app.config.HibernateConfig;
-import app.dao.GenericDAO;
-import app.entities.Movie;
-import app.services.EntityService;
-import jakarta.persistence.EntityManagerFactory;
-
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-      //  AppRunner appRunner = new AppRunner();
-      //  appRunner.run();
-
-        EntityService entityService = new EntityService();
-        List<Movie> movieList = entityService.convertToMovieEntities();
-
-       // System.out.println(movieList.size());
-
-        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
-
-        GenericDAO genericDAO = new GenericDAO(emf, Movie.class);
-
-        for (Movie movie : movieList) {
-            genericDAO.create(movie);
-        }
-
-
+        AppRunner appRunner = new AppRunner();
+        appRunner.run();
     }
 }
