a2d84a2cac7077f1c3cb1957eb402b9ee5376a18
KatarinaKN <katarinak.nielsen@gmail.com>
2026-09-20 10:58:25 +0200

    Main: added code to run fetch and save methods and to run print methods.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
 import app.AppRunner;
+import app.services.EntityService;
+import app.views.MoviePrinter;
 
 public class Main {
 
     public static void main(String[] args) {
-        AppRunner appRunner = new AppRunner();
-        appRunner.run();
+       /* AppRunner appRunner = new AppRunner();
+        appRunner.run(); */
+
+        //Gemmer fetchet API-data i egen database.
+      /*  EntityService entityService = new EntityService();
+        entityService.saveMoviesInDB(); */
+
+        //Printer data fra egen database.
+        MoviePrinter moviePrinter = new MoviePrinter();
+
+     //   moviePrinter.printAllMovies();
+     //   moviePrinter.printGenres();
+     //   moviePrinter.printFindMovieByKeyword("sommer");
+     // OBS! Virker ikke.   moviePrinter.printFindMoviesByGenre("Thriller");
+     //   moviePrinter.printTopTenHighestRating();
+     //   moviePrinter.printTopTenLowestRating();
     }
 }
